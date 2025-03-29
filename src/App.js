38cb45c0ffac25838fa0a90f1cfadf8c699d2347
parkpoto0404 <parkpoto@gmail.com>
2025-03-29 12:54:56 +0900

    scss 셋팅 및 페이지 생성중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import Main from './page/Main';
+import Login from './page/Login';
+import Pwdfind from './page/Pwdfind';
+import Signup from './page/Signup';
+
+
 
 
 function App() {
   return (
     <div className="App">
-      안녕
+      
+      <BrowserRouter>
+            <Routes>
+              <Route path='/main' element={<Main />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/pwdfind' element={<Pwdfind />} />
+              <Route path='/signup' element={<Signup />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
