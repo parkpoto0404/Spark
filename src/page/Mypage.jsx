7d03a9be3a78027f72f2e57708d4b466b11a31c5
parkpoto0404 <parkpoto@gmail.com>
2025-04-15 12:48:59 +0900

    로그아웃 로직 구현

--- src/page/Mypage.jsx
+++ src/page/Mypage.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
 
 const Mypage = () => {
+
+  const navigate = useNavigate();
+  const { setLoginCheck } = useAuthContext();
+  const refreshToken = localStorage.getItem('refreshToken');
+
+  
+  const logingOut= async ()=>{
+    
+    const confirmLogout = window.confirm('로그아웃 하시겠습니까?');
+    
+    if(confirmLogout){
+      
+      try{
+        const res = await fetch('http://localhost:8888/spark/api/logout', {
+          method : 'POST',
+          headers : {
+            'Content-Type' : 'application/json', // json 형식으로 보내기
+          },
+          body : JSON.stringify({refreshToken}),
+          credentials : 'include'
+  
+        });
+
+        if(res.ok){
+          // 서버에서 처리 성공
+          localStorage.removeItem('jwt'); // access token 제거
+          localStorage.removeItem('refreshToken'); // refresh token 제거
+          setLoginCheck(false); // 로그인 상태 false로 초기화
+          navigate('/login'); // 로그인 페이지로 리다이렉트
+          console.log('로그인 성공')
+        } else {
+          console.log('로그아웃 실패');
+        }
+  
+      } catch (err){
+        console.error('로그아웃 요청 실패:', err);
+      }
+
+    };
+
+  };
+  
+
+
   return (
     <div>
-       마이페이지
+       <button onClick={logingOut}>로그아웃</button>
     </div>
   )
 }
