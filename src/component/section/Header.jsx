9021cb8a23e78117c9d6e02fdd69f304a5e73523
parkpoto0404 <parkpoto@gmail.com>
2025-05-26 22:13:25 +0900

    상세페이지 css

--- src/component/section/Header.jsx
+++ src/component/section/Header.jsx
@@ -6,7 +6,7 @@ import { useAuthContext } from "../../context/AuthContext";
 const Header = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const { step , setStep } = useAuthContext();
+  const { step, setStep } = useAuthContext();
 
   const { recommendations, scrollY } = location.state || {}; // 메인에서 봤던 리스트와 스크롤위치
 
@@ -14,72 +14,72 @@ const Header = () => {
   const insertInfoPage = location.pathname === '/insertInfo';
   const mainPage = location.pathname === '/';
   const detailPage = location.pathname === '/detail';
-  
 
-  
+
+
   const goBackToHome = () => { // 상세보기에서 메인으로 뒤로가기 핸들러
-    
+
     navigate("/", {
       state: {
-        recommendations : recommendations,
+        recommendations: recommendations,
         scrollY: scrollY,
       },
     });
-    sessionStorage.setItem('backFromDetail','true');
+    sessionStorage.setItem('backFromDetail', 'true');
   };
 
   const handleBack = () => { // 정보입력페이지 뒤로가기 핸들러
     setStep(prev => prev > 1 ? prev - 1 : prev); // 1보다 클 때만 감소
-    
+
   }
 
   return (
     <div className='header'>
-      {isSignupPage&& (
-        <div style={{display: 'flex'}}>
+      {isSignupPage && (
+        <div style={{ display: 'flex' }}>
           <div className='back-btn'>
-            <span onClick={() => navigate(-1)}> 
-              <h2>〈 </h2> 
+            <span onClick={() => navigate(-1)}>
+              <h2>〈 </h2>
             </span>
           </div>
-          <span style={{alignContent: 'center'}}><h2>회원가입</h2></span>
+          <span style={{ alignContent: 'center' }}><h2>회원가입</h2></span>
         </div>
       )}
 
-      {detailPage&& (
-        <div style={{display: 'flex'}}>
+      {detailPage && (
+        <div style={{ display: 'flex' }}>
           <div className='back-btn'>
-            <span onClick={()=> goBackToHome()}> 
-              <h2>〈 </h2> 
+            <span onClick={() => goBackToHome()}>
+              <h2>〈 </h2>
             </span>
           </div>
-          <span style={{alignContent: 'center'}}><h2>상세보기</h2></span>
+          <span style={{ alignContent: 'center' }}><h2>상세보기</h2></span>
         </div>
       )}
-        
-      {mainPage&& (
-        <div style={{display: 'flex'}}>
-          <span style={{alignContent: 'center'}}><h2>소개팅 추천</h2></span>
+
+      {mainPage && (
+        <div style={{ display: 'flex' }}>
+          <span style={{ alignContent: 'center' }}><h2>소개팅 추천</h2></span>
         </div>
       )}
-      
-      {insertInfoPage&& (
-        <div style={{display: 'flex'}}>
+
+      {insertInfoPage && (
+        <div style={{ display: 'flex' }}>
           <div className='back-btn' >
-            <span onClick={handleBack}> 
-              <h2>〈 </h2> 
+            <span onClick={handleBack}>
+              <h2>〈 </h2>
             </span>
           </div>
-          <span style={{alignContent: 'center'}}><h4> 작성완료까지 {step}/11</h4></span>
-          <span style={{alignContent: 'center'}}></span>
+          <span style={{ alignContent: 'center' }}><h4> 작성완료까지 {step}/11</h4></span>
+          <span style={{ alignContent: 'center' }}></span>
         </div>
       )}
-      
-      
+
+
 
 
     </div>
-    
+
   );
 };
 
--- src/page/Detail.jsx
+++ src/page/Detail.jsx
@@ -1,30 +1,48 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { IoLocationSharp } from "react-icons/io5";
+import { PiBagSimpleFill } from "react-icons/pi";
+
 
 const Detail = () => {
-  const location = useLocation();
-  
+    const location = useLocation();
+
+    const { user } = location.state;
 
 
-  const { user } = location.state;
+    if (!user) return <div>사용자 정보가 없습니다.</div>;
 
-  
-  if (!user) return <div>사용자 정보가 없습니다.</div>;
+    console.log('user', user)
 
-  console.log('user',user)
+    return (
+        <div className="container">
+            <div className="profile-card">
+                <img className="profile-image" src={`http://localhost:8888${user.proFile}`} alt="프로필" />
+            </div>
 
-  return (
-    <div>
-      <h2>{user.nickName}님의 프로필</h2>
-      <img src={`http://localhost:8888${user.proFile}`} alt="프로필" />
-      <p>자기소개: {user.memInfo}</p>
-      <p>나이: {user.age}</p>
-      <p>학력 : {user.education} </p>
-      <p>직업: {user.occupation}</p>
-      <p>지역: {user.location}</p>
-     
-    </div>
-  );
+            <div className="profile-header">
+                <div style={{ display: 'flex' }}>
+                    <h2>{user.nickName}&nbsp;&nbsp;{user.age}</h2>
+                </div>
+                <span style={{ marginTop: "10px", display: "flex" }}>
+                    <PiBagSimpleFill style={{ alignSelf: "center", color: "#ccc" }} />
+                    <p style={{ marginLeft: '10px' }}>{user.occupation}</p>
+                </span>
+                <span style={{ display: "flex" }}>
+                    <IoLocationSharp style={{ alignSelf: "center", color: "#ccc" }} />
+                    <p style={{ marginLeft: '10px' }}>{user.location}</p>
+                </span>
+            </div>
+            
+            <div>
+                <div>
+                    <h2>기본정보</h2>
+                </div>
+            </div>
+            
+
+        </div>
+    );
 };
 
 export default Detail;
